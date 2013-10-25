90d13499ca4dd79bb32ff1876b63ac2f4b8d0490
yang-lu <yang.lu@outlook.com>
2013-10-24 22:43:55 -0700

    Update findStars.cs

--- ArraysAndStrings/findStars.cs
+++ ArraysAndStrings/findStars.cs
@@ -1,3 +1,10 @@
+/* No.6 有N个人，其中k个明星和N-k个群众，N很大，k很小为O(1)。
+   所有人都认识明星，明星不认识任何群众，群众之间可能认识，
+   现在你是R2D2，你每次问一个人是否认识另一人的代价为O(1)，
+   试设计一种算法找出明星，并给出时间复杂度。
+ */
+
+
 public List<Guy> FindFamousGuys(Guy[] guys)
 {
 　List<Guy> famousGuys = new List<Guy>();
